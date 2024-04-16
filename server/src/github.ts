598c09783c1c2272fcb3a536bb697f7ca3b2d857
Gabriel Freitas Dev <gabrielfreitas.ilustra@gmail.com>
2024-04-16 18:56:59 -0300

    feat: return user list from api, add search and pagenator

--- server/src/github.ts
+++ server/src/github.ts
@@ -6,12 +6,31 @@ const router = express.Router();
 router.get('/users', async (req: Request, res: Response) => {
   try {
     const searchTerm = req.query.term as string;
-    const response = await axios.get(`https://api.github.com/search/users?q=${searchTerm}`);
-    const users = response.data.items;
-    res.json(users);
+    const page = req.query.page ? parseInt(req.query.page as string) : 1; // Página padrão é 1
+    const perPage = 4; // Número padrão de usuários por página
+
+    if (!searchTerm) {
+      return res.status(400).send('O parâmetro de termo de busca não foi fornecido.');
+    }
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${searchTerm}&page=${page}&per_page=${perPage}`,
+    );
+    const { total_count, items } = response.data; // Extrai o total de usuários e os usuários da resposta
+
+    const totalPages = Math.ceil(total_count / perPage); // Calcula o número total de páginas
+
+    const paginationInfo = {
+      totalItems: items.length,
+      totalPages: totalPages,
+      currentPage: page,
+      perPage: perPage,
+    };
+
+    res.json({ users: items, pagination: paginationInfo }); // Retorna os usuários e informações de paginação
   } catch (error) {
     console.error('Erro ao buscar usuários do GitHub:', error);
-    res.status(500).json({ error: 'Erro ao buscar usuários do GitHub' });
+    res.status(500).send('Erro ao buscar usuários do GitHub. Por favor, tente novamente mais tarde.');
   }
 });
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,70 +1,107 @@
-import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, Search } from 'lucide-react';
+import { useState } from 'react';
 import DefaultAvatar from '/src/assets/defaulAvatar.png';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [users, setUsers] = useState([]);
+  const [pagination, setPagination] = useState({
+    totalItems: 0,
+    totalPages: 0,
+    currentPage: 1,
+    perPage: 10,
+  });
+  const [error, setError] = useState('');
+
+  const searchUsers = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/users?term=${searchTerm}`);
+      if (response.ok) {
+        const data = await response.json();
+        setUsers(data.users);
+        setPagination(data.pagination);
+        setError('');
+      } else {
+        const errorMessage = await response.text();
+        setError(errorMessage);
+      }
+    } catch (error) {
+      setError('Erro ao buscar usuários. Por favor, tente novamente mais tarde.');
+    }
+  };
+
+  const goToPage = async (page: number) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/users?term=${searchTerm}&page=${page}`);
+      if (response.ok) {
+        const data = await response.json();
+        setUsers(data.users);
+        setPagination(data.pagination);
+        setError('');
+      } else {
+        const errorMessage = await response.text();
+        setError(errorMessage);
+      }
+    } catch (error) {
+      setError('Erro ao buscar usuários. Por favor, tente novamente mais tarde.');
+    }
+  };
+
   return (
     <>
       <div className="w-full max-w-4xl mx-auto p-5 flex flex-col gap-5">
         <div className="flex flex-col gap-4">
           <div className="flex gap-3 items-center justify-between">
             <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
-              <Search className="size-4 text-emerald-300" />
               <input
                 className="bg-transparent flex-1 outline-none border-0 p-0 text-sm"
                 placeholder="Buscar usuários..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
               />
+              <button onClick={searchUsers}>Buscar</button>
             </div>
           </div>
 
-          <div className="w-full">
-            <ul className="flex flex-col gap-4">
-              {Array.from({ length: 4 }).map((_, i) => {
-                return (
-                  <li
-                    key={i}
-                    className="flex items-center justify-between flex-wrap py-3 px-4 border border-white/10 rounded-lg"
-                  >
-                    <div className="flex items-center">
-                      <img className="w-10 h-10 rounded-full mr-4" src={DefaultAvatar} alt="Avatar of..." />
-                      <div className="text-sm flex flex-col">
-                        <a href="#" className="font-semibold text-base leading-none hover:underline">
-                          Jonathan Reinink
-                        </a>
-                        <span className="text-sm font-light">jonathanReinink</span>
-                      </div>
-                    </div>
-                    <div className="flex flex-col">
-                      <span className="text-sm">5 followers</span>
-                      <span className="text-sm">9 following</span>
-                      <span className="text-sm">87 Repositorios</span>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div className="py-3 px-4 border border-white/10 rounded-lg">
-            <div className="flex items-center justify-between">
-              <div>
-                <span className="text-sm">Showing 10 of 228 items</span>
-              </div>
-              <div className="inline-flex items-center gap-8">
-                <span className="text-sm">Page 1 of 23</span>
+          {error && <div className="text-sm py-3 px-4 border border-rose-500 rounded-lg text-rose-500">{error}</div>}
 
-                <div className="flex gap-1.5">
-                  <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-                    <ChevronsLeft className="size-4" />
-                  </button>
-                  <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-                    <ChevronLeft className="size-4" />
-                  </button>
-                  <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-                    <ChevronRight className="size-4" />
-                  </button>
-                  <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-                    <ChevronsRight className="size-4" />
-                  </button>
-                </div>
+          {users.map((user: any) => (
+            <div key={user.id} className="py-3 px-4 border border-white/10 rounded-lg">
+              <div className="flex items-center">
+                <img
+                  className="w-10 h-10 rounded-full mr-4"
+                  src={user.avatar_url || DefaultAvatar}
+                  alt={`Avatar de ${user.login}`}
+                />
+                <span className="text-sm font-light">{user.login}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="py-3 px-4 border border-white/10 rounded-lg">
+          <div className="flex items-center justify-between">
+            <div>
+              <span className="text-sm">
+                Mostrando {users.length} de {users.length} itens
+              </span>
+            </div>
+            <div className="inline-flex items-center gap-8">
+              <div className="flex gap-1.5">
+                <button
+                  className="bg-white/10 border border-white/10 rounded-md p-1.5"
+                  onClick={() => goToPage(pagination.currentPage - 1)}
+                  disabled={pagination.currentPage === 1}
+                >
+                  <ChevronLeft className="size-4" />
+                </button>
+                <button
+                  className="bg-white/10 border border-white/10 rounded-md p-1.5"
+                  onClick={() => goToPage(pagination.currentPage + 1)}
+                  disabled={pagination.currentPage === pagination.totalPages}
+                >
+                  <ChevronRight className="size-4" />
+                </button>
               </div>
             </div>
           </div>
